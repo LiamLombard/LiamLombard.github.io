ea92f4a6e43baf711d6a8be6b54fd38faec41c92
LiamLombard <lplombard@hotmail.co.uk>
2017-11-29 21:41:08 +0000

    fixed autofocus again

--- scripts/startPage.js
+++ scripts/startPage.js
@@ -187,5 +187,4 @@ $(document).ready(function(){
     document.getElementById("search-bar").addEventListener("keydown", function(e) {
         if (e.keyCode == 13) { newSearch(); }
     }, false);
-    $("#field").focus();
 });
